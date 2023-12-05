b6085cef10aeabd23934ebd3c04d2e26f69c4c1e
Morrigan <107103167+corvidfox@users.noreply.github.com>
2023-12-04 20:04:02 -0600

    Update __init__.py
    Signed-off-by: Morrigan <107103167+corvidfox@users.noreply.github.com>

--- pubmed_tool/__init__.py
+++ pubmed_tool/__init__.py
@@ -1,10 +1,3 @@
 """
 Root of pubmed tool - does not need content
 """
-
-from pubmed_tool import *
-import vis_funct as vis
-import validators
-import config_logging
-import sql_functs as sql
-import scraper_functs as scr
