246cc53209724e5ddc3b0dc2b9159fe6213881bb
Morrigan Mahady <morri.mahady@gmail.com>
2023-12-04 20:19:45 -0600

    Attempting to namespace

--- pubmed_tool/__init__.py
+++ pubmed_tool/__init__.py
@@ -1,3 +1,9 @@
 """
 Root of pubmed tool - does not need content
 """
+from .pubmed_tool import *
+import config_logging
+import validators
+import vis_functs as vis
+import sql_functs as sql
+import scraper_functs as scr
